4a24b1109fdf220838e5467020263031bb26522e
GabriellaFaludi <gabriella.faludi@awacademy.com>
2022-10-12 11:12:32 +0200

    Înitial commit

---
+++ WarmUp.java
@@ -0,0 +1,14 @@
+public class WarmUp {
+
+    public static public static void main(String[] args) {
+
+        int [] array1;
+        array1 = new int[3];
+
+        String[] array2;
+        array2 = new  String[3];
+
+
+
+    }
+}
